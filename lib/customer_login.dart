5148ccd8653a7ef2cd585be3dd13d731bd75860e
insaiyancvk <cvamshik1@gmail.com>
2021-03-22 01:35:24 +0530

    customer login page fully functioning with google sign in

--- lib/customer_login.dart
+++ lib/customer_login.dart
@@ -1,19 +1,26 @@
 import 'dart:ui';
-
+import 'package:expiry_remainder/Screens/customer_dashboard.dart';
+import 'package:expiry_remainder/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
+import 'package:flutter_svg_provider/flutter_svg_provider.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class CustomerLoginPage extends StatelessWidget {
+class CustomerLoginPage extends StatefulWidget {
+  @override
+  _CustomerLoginPageState createState() => _CustomerLoginPageState();
+}
+
+class _CustomerLoginPageState extends State<CustomerLoginPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         image: const DecorationImage(
-            image: AssetImage('assets/loginc.jpg'), fit: BoxFit.cover),
+            image: Svg('assets/signin.svg'), fit: BoxFit.fitHeight),
       ),
       child: BackdropFilter(
-        filter: ImageFilter.blur(sigmaX: 2.5, sigmaY: 2.5),
+        filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
         child: Scaffold(
           backgroundColor: Colors.transparent,
           body: Center(
@@ -27,7 +34,7 @@ class CustomerLoginPage extends StatelessWidget {
                   padding: const EdgeInsets.fromLTRB(75.0, 0, 0, 20),
                   child: Text('Sign in as customer',
                       style: GoogleFonts.fugazOne(
-                          color: Colors.white,
+                          color: Color(0xFFF2AA4C),
                           fontSize: 55,
                           fontWeight: FontWeight.w200)),
                 ),
@@ -35,17 +42,26 @@ class CustomerLoginPage extends StatelessWidget {
                   height: 25,
                 ),
                 SizedBox(
-                  width: (MediaQuery.of(context).size.width) / 1.5,
-                  height: (MediaQuery.of(context).size.height) / 20,
-                  child: GoogleSignInButton(
-                    splashColor: Colors.blue[900],
-                    onPressed: () {},
-                    darkMode: true,
-                    borderRadius: 50,
-                    textStyle: GoogleFonts.lato(
-                        fontSize: 20, fontWeight: FontWeight.w600),
-                  ),
-                )
+                    width: (MediaQuery.of(context).size.width) / 1.5,
+                    height: (MediaQuery.of(context).size.height) / 20,
+                    child: GoogleSignInButton(
+                      splashColor: Colors.blue[900],
+                      onPressed: () {
+                        signInWithGoogle().whenComplete(() {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) {
+                                return CustomerDashboard();
+                              },
+                            ),
+                          );
+                        });
+                      },
+                      darkMode: true,
+                      borderRadius: 50,
+                      textStyle: GoogleFonts.lato(
+                          fontSize: 20, fontWeight: FontWeight.w600),
+                    ))
               ],
             ),
           ),
