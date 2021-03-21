f6cc9f418f5733e8eb8631e039c13be09db89420
insaiyancvk <cvamshik1@gmail.com>
2021-03-21 17:00:47 +0530

    Complete home screen, finish sign in as customer page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:ui';
+import 'package:expiry_remainder/customer_login.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -7,56 +11,113 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      themeMode: ThemeMode.dark,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Colors.transparent,
+        textTheme: GoogleFonts.latoTextTheme(),
+        primaryColor: Colors.white,
+        primaryColorBrightness: Brightness.light,
+        brightness: Brightness.light,
+        primaryColorDark: Colors.black87,
+        canvasColor: Colors.white,
+        // next line is important!
+        appBarTheme: AppBarTheme(brightness: Brightness.light),
       ),
-      home: MyHomePage(),
+      darkTheme: ThemeData(
+        primaryColor: Colors.deepPurpleAccent,
+        primaryColorBrightness: Brightness.dark,
+        primaryColorLight: Colors.black87,
+        brightness: Brightness.dark,
+        primaryColorDark: Colors.black87,
+        indicatorColor: Colors.white,
+        canvasColor: Colors.black87,
+        // next line is important!
+        appBarTheme: AppBarTheme(brightness: Brightness.values[0]),
+      ),
+      home: SigninPage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class SigninPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('sign in'),
-        flexibleSpace: Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: <Color>[
-                    Color.fromRGBO(42,105,173,1.0),
-                    Color.fromRGBO(41,115,183,1.0),
-                    Color.fromRGBO(38,137,194,1.0),
-                    Color.fromRGBO(35,164,206,1.0),
-                    Color.fromRGBO(32,189,219,1.0),
-                    Color.fromRGBO(32,213,234,1.0),
-                    Color.fromRGBO(28,235,245,1.0),
-                    Color.fromRGBO(27,255,254,1.0)
-
-                  ])
-          ),
-        ),
+    return Container(
+      decoration: BoxDecoration(
+        image: const DecorationImage(
+            image: AssetImage('assets/homebg.jpg'), fit: BoxFit.fill),
       ),
-      body: Center(
-        child: Container(
-          child: Center(
-            child: Text(
-              'Hello Gradient!',
-              style: TextStyle(
-                fontSize: 48.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 2.5, sigmaY: 2.5),
+        child: Scaffold(
+            backgroundColor: Colors.transparent,
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: (MediaQuery.of(context).size.height) / 4.5,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(75, 0, 0, 40),
+                    child: Text("Expiry Reminder",
+                        style: GoogleFonts.fugazOne(
+                            fontSize: 50, fontWeight: FontWeight.bold)),
+                  ),
+                  SizedBox(
+                      width: (MediaQuery.of(context).size.width) / 1.5,
+                      height: (MediaQuery.of(context).size.height) / 20,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                            foregroundColor:
+                                MaterialStateProperty.all<Color>(Colors.black),
+                            backgroundColor:
+                                MaterialStateProperty.all<Color>(Colors.white),
+                            shape: MaterialStateProperty.all<
+                                    RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(50)))),
+                        child: Text("Sign in as customer",
+                            style: GoogleFonts.lato(
+                                fontSize: 20, fontWeight: FontWeight.w600)),
+                        onPressed: () => gotoCustomerLogin(context),
+                      )),
+                  SizedBox(
+                    height: (MediaQuery.of(context).size.height) / 20,
+                  ),
+                  SizedBox(
+                    width: (MediaQuery.of(context).size.width) / 1.5,
+                    height: (MediaQuery.of(context).size.height) / 20,
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                          foregroundColor:
+                              MaterialStateProperty.all<Color>(Colors.black),
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.white),
+                          shadowColor:
+                              MaterialStateProperty.all<Color>(Colors.black),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(50)))),
+                      onPressed: () {},
+                      child: Text(
+                        "Sign in as seller",
+                        style: GoogleFonts.lato(
+                            fontSize: 20, fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
-          ),
-        ),
+            )),
       ),
     );
   }
+}
+
+Future gotoCustomerLogin(context) async {
+  Navigator.push(
+      context, MaterialPageRoute(builder: (context) => CustomerLoginPage()));
 }
